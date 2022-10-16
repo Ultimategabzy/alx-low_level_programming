587ee3bf46bb8efa9b8f6b74d20c5305ec130e6d
Ultimategabzy <treasurebasecafe@gmail.com>
2022-10-16 16:24:47 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase "programming is likr building a multilingual puzzle"
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
