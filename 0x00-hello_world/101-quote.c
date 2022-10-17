c83b4dfa05b42f54bab9c421af8d63f9555b556d
Ultimategabzy <treasurebasecafe@gmail.com>
2022-10-17 14:21:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (success)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ *   followed by a new line, to the standard error.
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
 {
-	write(2, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is userful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
