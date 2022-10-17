73df1bd59ce30136d7bf3c1198f2b2887a336a91
Ultimategabzy <treasurebasecafe@gmail.com>
2022-10-17 08:33:35 -0700

    5-Print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar,
+ * main - print the phrase with proper grammar,
  *but the outcome is a piece of art,
- * Returns: 0 if exitedproperly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
