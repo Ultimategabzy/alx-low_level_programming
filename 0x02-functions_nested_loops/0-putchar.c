6cbd6416d8983ecb4656995b2463a0e269c450bf
Ultimategabzy <treasurebasecafe@gmail.com>
2022-10-19 06:46:24 -0700

    0_putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,2 @@
-#include "main.h"
+#include "main.c"
 
-/**
- * main - prints putchar followed by a newline
- * Return: returns 0
- */
-int main(void)
-{
-	char str[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-		_putchar(str[1]);
-
-	_putchar('\n');
-
-	return (0);
-}
